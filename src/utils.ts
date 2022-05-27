ba036778ba7c30577ed92a53db4150de0a28c64f
Chenyt26 <2396164919@qq.com>
2022-05-27 10:08:24 +0800

    base64 decode
    Match-id-a3c73a6989acf6c26f4b870021aef4fc39ce3da5

--- src/utils.ts
+++ src/utils.ts
@@ -25,3 +25,38 @@ export function checkRegistryList(registryList: string[]): boolean {
     }
     return true;
 }
+
+/**
+ * 检查authList镜像列表参数是否合法
+ * @param string[]
+ * @returns boolean
+ */
+ export function checkAuthList(authList: string[]): boolean {
+  for (let i = 0; i < authList.length; i++) {
+      console.log(authList[i]);
+      const authReg = new RegExp(/^_auth=.+/);
+      const scopeAuthReg = new RegExp(/^\/\/.+:_auth=.+/);
+      if (!authReg.test(authList[i]) && !scopeAuthReg.test(authList[i])) {
+          return false;
+      }
+      const splitArray = authList[i].split('_auth='); 
+      const authBase64 = splitArray[1]; 
+      console.log(authBase64);
+  }
+  return true;
+}
+
+/**
+ * 判断字符是否是base64编码
+ * @param base64String 
+ * @returns boolean
+ */
+export function isBase64(base64String: string) {
+  if (base64String ==='' || base64String.trim() ===''){ return false; }
+  try {
+      const preString =  Buffer.from(base64String, 'base64').toString();
+      return Buffer.from(preString).toString('base64') == base64String;
+  } catch (err) {
+      return false;
+  }
+}
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,20 +1,35 @@
 import * as utils from '../src/utils';
 
-describe('test whether the registry list is vaild', () => {
+// describe('test whether the registry list is vaild', () => {
+//     const testCase = [
+//         {arr: [], result: true},
+//         {arr: ['registry=hh'], result: true},
+//         {arr: ['registry='], result: false},
+//         {arr: ['@test:registry='], result: false},
+//         {arr: ['@test:registry=dsads'], result: true},
+//         {arr: ['@:registry=dsads'], result: false},
+//         {arr: ['@test:=dsads'], result: false},
+//         {arr: ['sdsd'], result: false}
+//     ];
+//     testCase.forEach(item => {
+//         const {arr, result} = item;
+//         test(`registryList输入为(${arr}), json array判断结果：${result}`, async () => {
+//             expect(utils.checkRegistryList(arr)).toBe(result);
+//         });
+//     });
+// });
+
+describe('test whether the string is base64', () => {
     const testCase = [
-        {arr: [], result: true},
-        {arr: ['registry=hh'], result: true},
-        {arr: ['registry='], result: false},
-        {arr: ['@test:registry='], result: false},
-        {arr: ['@test:registry=dsads'], result: true},
-        {arr: ['@:registry=dsads'], result: false},
-        {arr: ['@test:=dsads'], result: false},
-        {arr: ['sdsd'], result: false}
+        {base64String: 'aXNCYXNlNjQ=', result: true},
+        {base64String: '', result: false},
+        {base64String: ' ', result: false},
+        {base64String: 'hello', result: false},
     ];
     testCase.forEach(item => {
-        const {arr, result} = item;
-        test(`registryList输入为(${arr}), json array判断结果：${result}`, async () => {
-            expect(utils.checkRegistryList(arr)).toBe(result);
+        const {base64String, result} = item;
+        test(`字符输入为(${base64String}), base64字符判断结果：${result}`, async () => {
+            expect(utils.isBase64(base64String)).toBe(result);
         });
     });
 });
